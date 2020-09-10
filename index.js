c921832fdcb08fff04f1df3b0318f019fb9c53ee
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-10 18:39:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  const array=[];
+  for(let i=0; i<musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+  let i = 0;
+  const newArray=[];
+  while(i<facts.length){
+    newArray.push(`${facts[i]}` + "!!!");
+    i++;
+  }
+  return newArray;
+}
+
+
+function iLoveTheBeatles(number){
+  let i = 0;
+  const array=[];
+  function incrementVariable(){
+   i = i + 1;
+    
+  }do{
+    array.push("I love the Beatles!");
+  }while(number<15);
+  i++
+  return array;
+}
+
